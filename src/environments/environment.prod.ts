6a56798c01f5de2b883e7985e8db1fa9cbbb7d7c
carloscalderon <cdcalderon@gmail.com>
2017-08-12 12:58:43 -0500

    Fix prod environment var names to match Dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  stockMarketQuotesWithIndicatorsApi: 'https://warm-journey-46979.herokuapp.com',
-  stockMarketUDFApi: 'https://enigmatic-waters-56889.herokuapp.com'
+  stockMarketQuotesWithIndicatorsApiBaseUrl: 'https://warm-journey-46979.herokuapp.com',
+  stockMarketUDFApiBaseUrl: 'https://enigmatic-waters-56889.herokuapp.com'
 };
