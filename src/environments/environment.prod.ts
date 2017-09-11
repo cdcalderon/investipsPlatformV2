71f053cc6b30e030ea7c72715099b642e91bea33
carloscalderon <cdcalderon@gmail.com>
2017-09-11 16:30:44 -0500

    add investipsServerWebAPIBaseUrl Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   stockMarketQuotesWithIndicatorsApiBaseUrl: 'https://warm-journey-46979.herokuapp.com',
-  stockMarketUDFApiBaseUrl: 'https://enigmatic-waters-56889.herokuapp.com'
+  stockMarketUDFApiBaseUrl: 'https://enigmatic-waters-56889.herokuapp.com',
+  investipsServerWebAPIBaseUrl: 'http://localhost:3308'
 };
